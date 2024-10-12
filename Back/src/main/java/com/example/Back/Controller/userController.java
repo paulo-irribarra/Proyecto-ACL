e360c845ac218ff64de607d922650e020d001e05
malikmassoud <pau.irribarra@duocuc.cl>
2024-10-12 17:16:51 -0300

    commit inicial del proyecto

---
+++ Back/src/main/java/com/example/Back/Controller/userController.java
@@ -0,0 +1,9 @@
+package com.example.Back.Controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+
+}
---
+++ Back/src/main/java/com/example/Back/ProyectoBackApplication.java
@@ -0,0 +1,14 @@
+package com.example.Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBackApplication.class, args);
+	}
+
+
+}
